e3d7c6897f4918484dbdb776126d5eefd1ace6d7
Md Momin Ali <momin550550@gmail.com>
2026-07-02 08:33:48 +0600

    update next.config.ts file

--- frontend-admin/next.config.ts
+++ frontend-admin/next.config.ts
@@ -4,7 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "bd-craft-ecommerce.onrender.com",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
   },
--- frontend-user/next.config.ts
+++ frontend-user/next.config.ts
@@ -4,10 +4,25 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "bd-craft-ecommerce.onrender.com",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
   },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "bd-craft-ecommerce.onrender.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
